5184b22af7e94b9e18b22bd2b61213d62576e698
anuradha008 <121505265+anuradha008@users.noreply.github.com>
2024-03-29 10:31:22 +0000

    operation Done

--- 01_basics/conversation.js
+++ 01_basics/conversation.js
@@ -1,27 +1,17 @@
-let no = "33"
-let number = Number(no)
+// let no = "33"
+// let number = Number(no)
 // console.log(typeof(no));
 // console.log(typeof(number));
-let var1 = '33abc'
-let var2 = Number(var1)
+// let var1 = '33abc'
+// let var2 = Number(var1)
 // console.log( var2)
-let value = 33
-let negValue = -value
-// console.log(negValue)
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2/3);
-// console.log(2%3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
-let score = false
-let score1 = Number(score)
+
+// 
+
+// let score = false
+// let score1 = Number(score)
 // console.log( score1)
 // console.log(typeof score1)
 
@@ -37,8 +27,8 @@ typeof => all above value is number
 
 */
 
-let isLogged =2;
-let booleanIsLogged = Boolean(isLogged)
+// let isLogged =2;
+// let booleanIsLogged = Boolean(isLogged)
 // console.log( booleanIsLogged)
 // console.log(typeof booleanIsLogged)
 
@@ -49,7 +39,45 @@ Conversion of differnt value into Boolean
 "hell0" =>true
 ""=> false
 */
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log( stringNumber)
- console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log( stringNumber)
+//  console.log(typeof stringNumber)
+
+ // ***************************** Operations ************************************
+
+
+
+//  let value = 3 
+//  let negValue = -value
+//  console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 ="hello"
+let str2 = " anuradha"
+let str3= str1+str2
+// console.log(str3);
+// console.log("1" + 2);
+
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); //122 => first is String 
+// console.log(1 + 2 + "2"); // 32 => first is number 
+// console.log(1 + "2" + 2); //122 
+
+// console.log(+true)
+// console.log(+"")
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+// console.log(num1)
+// console.log(num2)
+// console.log(num3)
+
+// let gameCounter =100
+// ++gameCounter;
+// console.log(gameCounter)

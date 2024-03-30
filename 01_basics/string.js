1665f6e20c065a2c5c0a3a72b8c757b08b59ef91
anuradha008 <121505265+anuradha008@users.noreply.github.com>
2024-03-30 13:39:52 +0000

    String and it methods

---
+++ 01_basics/string.js
@@ -0,0 +1,21 @@
+// const name ="Anuradha"
+// console.log(name[1])
+// const repoCount = 50
+// console.log(`Hello my name is ${name} and my repo count id ${repoCount}`)
+
+
+const gameName = new String("Anuradha-V-hc")
+console.log(gameName.split('-'))
+// console.log(gameName.length)
+// console.log(gameName[0])
+// const newString = gameName.substring(0,4)
+// console.log(newString)
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString)
+// const newStringOne =" Anuradha "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+const url = "https://anuradha.com/anuradha%20%vidhale"
+
+console.log(url.replace('%20%' ,'-'))
+console.log(url.includes('yash'))

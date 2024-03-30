9a1f921853681805fecc6db706826a080bbed4b5
anuradha008 <121505265+anuradha008@users.noreply.github.com>
2024-03-30 09:15:32 +0000

    Heap and Stack learned

--- 01_basics/DataTypes-Notes.js
+++ 01_basics/DataTypes-Notes.js
@@ -7,7 +7,7 @@
 // 7 Types = > String , number , Boolean , Null , Undefined(declared not defined),Symbol(Used to make unique) ,BigInt
 const id = Symbol('123')
 const anotherId = Symbol('123')
-console.log(id == anotherId);
+// console.log(id == anotherId);
 
 
 
@@ -26,8 +26,33 @@ let myFunction= function(){
     console.log("Hello Anuradha");
 }
 
-console.log(typeof heros)  //object
-console.log(typeof myobj) //object
-console.log(typeof myFunction) //function
+// console.log(typeof heros)  //object
+// console.log(typeof myobj) //object
+// console.log(typeof myFunction) //function
 
 // Javascript is Dynamically typed Language
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Memory : Stack and Heap
+
+// Heap (Non - Primitive)
+
+let myYoutubeName = "Anuradhadotcom"
+let anothername = myYoutubeName
+myYoutubeName = "changeddotcom"
+
+console.log(myYoutubeName);
+console.log(anothername);
+
+
+// Stack (Primitive)
+
+let userOne ={
+    email :"user@google.com",
+    upi:"user@ybl"
+}
+let userTwo = userOne
+userOne.email="userChanged@gmail.com"
+console.log(userOne);
+console.log(userTwo);
